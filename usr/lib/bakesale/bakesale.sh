6085ed134d2e8beeb09b161003afae79a5ab696c
Matt Fawcett <matt.fawcett@me.com>
2023-09-01 19:44:53 +0100

    monitor443

--- usr/lib/bakesale/bakesale.sh
+++ usr/lib/bakesale/bakesale.sh
@@ -59,12 +59,36 @@ flush_nft_entries() {
 	done
 }
 
+# Add logging rules for 443 traffic to duiagnose BBC Sounds issues etc
+monitor443() {
+	# Check for existence of the logging rule in srcnat_wan
+	if ! nft list chain inet fw4 srcnat_wan | grep -q 'SNAT to 443 in srcnat_wan:'; then
+		nft insert rule inet fw4 srcnat_wan tcp dport 443 log prefix "\"SNAT to 443 in srcnat_wan: \""
+	fi
+
+	# Check for existence of the logging rule in srcnat_Net2Shield
+	if ! nft list chain inet fw4 srcnat_Net2Shield | grep -q 'SNAT to 443 in srcnat_Net2Shield:'; then
+		nft insert rule inet fw4 srcnat_Net2Shield tcp dport 443 log prefix "\"SNAT to 443 in srcnat_Net2Shield: \""
+	fi
+
+	# Check for existence of the logging rule in srcnat_Proton1VPN
+	if ! nft list chain inet fw4 srcnat_Proton1VPN | grep -q 'SNAT to 443 in srcnat_Proton1VPN:'; then
+		nft insert rule inet fw4 srcnat_Proton1VPN tcp dport 443 log prefix "\"SNAT to 443 in srcnat_Proton1VPN: \""
+	fi
+	if ! nft list chain inet fw4 reject_from_wan | grep -q 'ip saddr add @banIP:blocklistv4'; then
+		nft insert rule inet fw4 reject_from_wan meta nfproto ipv4 iifname { "eth0", "eth5" } log prefix "\"ip saddr add @banIP:blocklistv4 \""
+	fi
+	log notice "nftables monitor443 rules inserted"
+}
+
 # Setup the service
 setup() {
 	local action="$1"
 
 	cleanup_files
 	config_load bakesale || return 1
+	config_get_bool MONITOR443 global monitor443 1
+	(( MONITOR443 )) && { monitor443 || return 1; }
 	config_get_bool DEBUG global debug "$DEBUG"
 
 	local dev zone zones
