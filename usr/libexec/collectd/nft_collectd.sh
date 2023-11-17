3e19f3e5844a8c15ac39089ea22ddcb9940a4333
Matt Fawcett <matt.fawcett@me.com>
2023-11-17 14:46:08 +0000

    nft_collectd

--- usr/libexec/collectd/nft_collectd.sh
+++ usr/libexec/collectd/nft_collectd.sh
@@ -9,31 +9,33 @@ process_nftables() {
 	local chain jsn syn_flood drop_from_wan
 
 	chain="$1"
-	jsn=$(cat /tmp/nft-statistics.json) || return
-#nft list chain inet fw4 drop_from_wan
-#nft list chain inet fw4 synflood_alt
-#]
-#echo $jsn | jq . | grep -E 'comment|packets|chain'
-#exit
+	drop_from_wan=$(cat /tmp/nft-drop_from_wan.json) || return
+	syn_flood=$(cat /tmp/nft-syn_flood.json) || return
 
-	drop_from_wan=$(echo $jsn | jq '. | select(.comment == "Drop wan IPv4 SYN packets, no syslog")')
-	json_load "${drop_from_wan}"
+	json_load "${drop_from_wan}" || return
+    json_select nftables
+    json_select 3
+    json_select rule
     json_select expr
     json_select 4
     json_select counter
 	json_get_vars packets bytes
 	echo "PUTVAL \"$HOSTNAME/nft-$chain/syn_packets\" interval=$INTERVAL N:$packets"
 
-	drop_from_wan=$(echo $jsn | jq '. | select(.comment == "Drop any remaining wan IPv4 packets, no syslog")')
-	json_load "${drop_from_wan}"
+	json_load "${drop_from_wan}" || return
+    json_select nftables
+    json_select 4
+    json_select rule
     json_select expr
     json_select 3
     json_select counter
 	json_get_vars packets bytes
 	echo "PUTVAL \"$HOSTNAME/nft-$chain/wan_packets\" interval=$INTERVAL N:$packets"
 
-	syn_flood=$(echo $jsn | jq '. | select(.comment=="Drop excess SYN packets, count")')
-	json_load "${syn_flood}"
+	json_load "${syn_flood}" || return
+    json_select nftables
+    json_select 4
+    json_select rule
     json_select expr
     json_select 1
     json_select counter
